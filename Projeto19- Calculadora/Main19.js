f4ee5c2cd36c71ea0e184cdbb30310a94031b432
Artur A Pereira <121524915+ArturP28@users.noreply.github.com>
2023-01-29 16:00:45 -0300

    Calculadora funções básicas

---
+++ Projeto19- Calculadora/Main19.js
@@ -0,0 +1,19 @@
+function clicar(val) {
+    document.querySelector('#txt').value=val
+
+}
+
+function clicar(val) {
+    document.querySelector('#txt').value+=val
+}
+
+function c(val) {
+    document.querySelector('#txt').value=val
+}
+
+function igual() {
+    
+    document.querySelector('#txt').value = eval(document.querySelector('#txt').value);
+   
+}
+
