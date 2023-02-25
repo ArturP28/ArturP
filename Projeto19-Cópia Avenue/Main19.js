6998127fddddffde685d80b7b40d1addbe75df50
Artur A Pereira <121524915+ArturP28@users.noreply.github.com>
2023-02-24 23:33:50 -0300

    Cópia do modelo de  Página de login para app

---
+++ Projeto19-Cópia Avenue/Main19.js
@@ -0,0 +1,11 @@
+
+    var txt = document.querySelector('#email')
+    txt.addEventListener('mouseenter', entrou)
+
+    function entrou() {
+        alert('Página Ilustrativa')
+    }
+
+    function acessar() {
+        window.location.reload();
+    }
